9cdafe8513f7ef9c4524509e582f91b81caa875f
sue herrera <sueherrera30@gmail.com>
2018-09-07 13:14:04 -0500

    structure

---
+++ src/app/components/Item.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Item extends React.Component {
+    render() {
+        return(
+            <div>
+                <h5> I should be the header, but i dont have style yet... </h5>
+            </div>    
+        );
+    }
+}
+
+
--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Item } from "./components/Item";
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1> hello, Im a pretty newcomponent</h1>
+                 <Item/>
+                <h1> hello, Im a pretty newcomponent</h1>          
             </div>
         );
     }
 }
-
 render(<App/>, window.document.getElementById("canasta"));
